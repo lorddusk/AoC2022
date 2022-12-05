2dc1b3bcc98a9a9222738635a3984a092b9c531b
Tim Langen <tim.langen@driessen.nl>
2022-12-05 16:33:22 +0100

    Day 5, Part 1 WIP #2

--- day5.py
+++ day5.py
@@ -4,24 +4,24 @@ from utils.read import file
 def get_crates():
     string_input = file(5, "string")
 
-    string_input = ["    [D]    ",
-             "[N] [C]    ",
-             "[Z] [M] [P]",
-             "1   2   3",
-             "",
-             "move 1 from 2 to 1",
-             "move 3 from 1 to 3",
-             "move 2 from 2 to 1",
-             "move 1 from 1 to 2"]
+    # string_input = ["    [D]    ",
+    #          "[N] [C]    ",
+    #          "[Z] [M] [P]",
+    #          "1   2   3",
+    #          "",
+    #          "move 1 from 2 to 1",
+    #          "move 3 from 1 to 3",
+    #          "move 2 from 2 to 1",
+    #          "move 1 from 1 to 2"]
 
-    crates = [[] for i in range(3)]
+    crates = [[] for i in range(9)]
     assignments = [[] for i in range(3)]
 
     for line in string_input:
-        if not line.startswith("move") and not line.startswith("1"):
-            actual_line = line.replace("    ","X").replace(" ","").replace("[","").replace("]","")
+        if not line.startswith("move") and not line.startswith(" 1"):
+            actual_line = line.replace("    ",".").replace(" ","").replace("[","").replace("]","")
             for i in range(0, len(actual_line)):
-                if actual_line[i] != "X":
+                if actual_line[i] != ".":
                     crates[i].append(actual_line[i])
         if line.startswith("move"):
             assignment_line = line.replace("move ", "").replace("from ","").replace("to ","").split(" ")
@@ -29,18 +29,29 @@ def get_crates():
             assignments[1].append(assignment_line[1])
             assignments[2].append(assignment_line[2])
 
-        print(crates)
-        print(assignments)
-
-
-    # print(string_input)
-
-    # return crates, assignments
+    return crates, assignments
 
 
 def part_one():
-    get_crates()
-    print(f"The crate sequence that ends up on top is **{0}**")
+    crates, assignments = get_crates()
+    print(crates)
+    for i in range(0, len(assignments) + 1):
+        move = int(assignments[0][i])
+        _from = int(assignments[1][i]) - 1
+        to = int(assignments[2][i]) - 1
+        for j in range(move, 0, -1):
+            crate = crates[_from][0]
+            crates[_from].remove(crate)
+            crates[to].insert(0, crate)
+
+    print(crates)
+    string = ""
+    for x in crates:
+        try:
+            string += x[0]
+        except:
+            pass
+    print(f"The crate sequence that ends up on top is **{string}**")
 
 
 def part_two():
