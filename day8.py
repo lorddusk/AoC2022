c7dfb3694347a56929b4967839cc4515590a1ac7
lorddusk <lang2619@gmail.com>
2022-12-08 17:17:34 +0100

    Day 8, Part 1

--- day8.py
+++ day8.py
@@ -7,65 +7,21 @@ def get_inputs():
     return read_file(8, "string")
 
 
-class Forest:
-    def __init__(self, lines):
-        self.lines = lines
-        self.trees = []
-
-    def to_dict(self):
-        return {"trees": [tree.to_dict() for tree in self.trees]}
-
-    def visible(self):
-        int = 0
-        string = ""
-        for tree in self.trees:
-            if int == self.lines:
-                int = 0
-                string += "\n"
-            if tree.visible:
-                string += "O"
-            else:
-                string += "X"
-            int += 1
-
-        return string
-
-
-class Tree:
-    def __init__(self, x, y, height, visible=False):
-        self.height = height
-        self.x = x
-        self.y = y
-        self.visible = visible
-
-    def to_dict(self):
-        return {"pos": f"{self.x}, {self.y}", "height": self.height, "visible": self.visible}
-
-
-def get_example():
-    return ["30373",
-            "25512",
-            "65332",
-            "33549",
-            "35390"]
-
-
-def create_tree_grid(input):
-    forest = Forest(len(input))
-    for x in range(len(input)):
-        for y in range(len(input[x])):
-            if x == 0 or y == 0 or x == len(input) - 1 or y == len(input) - 1:
-                forest.trees.append(Tree(x, y, input[x][y], True))
-            else:
-                forest.trees.append(Tree(x, y, input[x][y]))
-    return forest
+def check_visible(data, row, column):
+    if all(data[row][column] > data[_row][column] for _row in range(row)):
+        return True
+    if all(data[row][column] > data[row][_column] for _column in range(column)):
+        return True
+    if all(data[row][column] > data[_row][column] for _row in range(row + 1, len(data))):
+        return True
+    if all(data[row][column] > data[row][_column] for _column in range(column + 1, len(data))):
+        return True
+    return False
 
 
 def part_one():
-    # input = get_example()
-    input = get_inputs()
-    print(create_tree_grid(input).visible())
-    total_trees = 0
+    data = get_inputs()
+    total_trees = sum([check_visible(data, row, column) for row in range(len(data)) for column in range(len(data))])
     print(f"There are **{total_trees}** trees visible from outside the grid")
 
 
