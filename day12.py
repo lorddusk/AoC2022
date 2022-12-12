978f52ea68d38313bdb80462b19714565faa2b60
Tim Langen <tim.langen@driessen.nl>
2022-12-12 16:26:11 +0100

    Day 12, Part 2

--- day12.py
+++ day12.py
@@ -12,9 +12,8 @@ def get_example():
     return read_file(f"{day}e", "string")
 
 
-def create_grid(data):
+def create_matrix(data):
     matrix = {}
-    grid = nx.DiGraph()
 
     for x, row in enumerate(data):
         for y, character in enumerate(row):
@@ -24,6 +23,10 @@ def create_grid(data):
     start = [key for key in matrix.keys() if matrix[key] == "S"][0]
     matrix[end] = "z"
     matrix[start] = "s"
+    return matrix, start, end
+
+def create_grid(matrix):
+    grid = nx.DiGraph()
 
     for (x, y) in matrix.keys():
         adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
@@ -31,19 +34,30 @@ def create_grid(data):
         adjacent = [pos for pos in adjacent if (ord(matrix[pos]) - ord(matrix[(x,y)])) <= 1]
         for adj in adjacent:
             grid.add_edge((x, y), adj)
-    return grid, start, end
+
+    return grid
 
 
 def part_one():
     data = get_inputs()
-    grid, start, end = create_grid(data)
+    matrix, start, end = create_matrix(data)
+    grid = create_grid(matrix)
     shortest_route = nx.shortest_path(grid, start, end)
     shortest_length = len(shortest_route)-1
     print(f"The shortest route to the endpoint is {shortest_length} steps")
 
 
 def part_two():
-    pass
+    data = get_inputs()
+    matrix, _, end = create_matrix(data)
+    grid = create_grid(matrix)
+    possible_candidates = [key for key in matrix.keys() if matrix[key] == "a"]
+    paths = []
+    for candidate in possible_candidates:
+        if nx.has_path(grid, candidate, end):
+            result = nx.shortest_path(grid, candidate, end)
+            paths.append(len(result)-1)
+    print(f"The shortest scenic route to the endpoint is {min(paths)} steps")
 
 
 if __name__ == "__main__":
