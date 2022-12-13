66835fc283fef3df8b9fcef4e5606a6e44a623d1
lorddusk <lang2619@gmail.com>
2022-12-13 18:39:32 +0100

    Day 13, Part 2

--- day13.py
+++ day13.py
@@ -1,3 +1,4 @@
+from functools import cmp_to_key
 from itertools import zip_longest
 
 from utils.read import read_file
@@ -18,9 +19,9 @@ def get_example():
 def compare(left, right):
     for result in compare_lists(left, right):
         if result == "good":
-            return True
+            return 1
         elif result == "bad":
-            return False
+            return -1
         elif result == "ok":
             continue
     return 0
@@ -64,6 +65,10 @@ def part_one():
     chunked_list = list(chunks(data, 2))
     for chunk in chunked_list:
         correct = compare(chunk[0], chunk[1])
+        if correct == 1:
+            correct = True
+        elif correct == -1:
+            correct = False
         pair_output.append({"pair": pair, "order": correct})
         pair += 1
     correct_pairs = sum([p['pair'] for p in pair_output if p['order']])
@@ -71,8 +76,14 @@ def part_one():
 
 
 def part_two():
-    data = get_example()
-    decoder_key = 0
+    data = get_inputs()
+    data.append([[2]])
+    data.append([[6]])
+    data.sort(key=cmp_to_key(compare), reverse=True)
+
+    key1 = data.index([[2]]) + 1
+    key2 = data.index([[6]]) + 1
+    decoder_key = key1 * key2
     print(f"The decoder key for the distress signal {decoder_key}")
 
 
